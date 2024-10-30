457f29e90444947dc5a2d5a7c1a573e3bcc10e66
Siddhesh Sule <64964929+siddheshsule@users.noreply.github.com>
2024-10-30 19:35:15 +0100

    Experience updated

--- data/experiences.ts
+++ data/experiences.ts
@@ -1,22 +1,62 @@
 export const experiences = [
   {
     id: 1,
-    period: "2021-Present",
+    period: "2023-Present",
     employer: "Marquardt GmbH",
     employerLocation: "Rietheim, Germany",
     position: "Technical Project Lead",
     jobDescription:
       "Spearhead end-to-end development of electric vehicle E-Lock systems, ensuring seamless progression from conceptualization to production.Collaborate with diverse teams, including electrical engineers, software developers, and designers, to define system requirements and ensure timely project delivery. Conduct comprehensive feasibility studies and technical analyses to design robust and reliable E-Lock solutions for electric vehicles.Manage the entire project lifecycle, overseeing planning, resource allocation, scheduling, and risk management to ensure successful outcomes and customer satisfaction.",
-    technologiesUsed: ['Python','React','C++', 'System Engineering'],
+    technologiesUsed: ["Python", "React", "C++", "System Engineering"],
   },
   {
-    id:2,
-    period: "2021-Present",
-    employer: "Marquardt GmbH",
-    employerLocation: "Rietheim, Germany",
-    position: "Technical Project Lead",
+    id: 2,
+    period: "2021-2022",
+    employer: "Merit Automotive Electronics Systems, S.L.U",
+    employerLocation: "Radolfzell, Germany",
+    position: "Technical Manager",
     jobDescription:
-      "Spearhead end-to-end development of electric vehicle E-Lock systems, ensuring seamless progression from conceptualization to production.Collaborate with diverse teams, including electrical engineers, software developers, and designers, to define system requirements and ensure timely project delivery. Conduct comprehensive feasibility studies and technical analyses to design robust and reliable E-Lock solutions for electric vehicles.Manage the entire project lifecycle, overseeing planning, resource allocation, scheduling, and risk management to ensure successful outcomes and customer satisfaction.",
-    technologiesUsed: ['Python','React','C++', 'System Engineering'],
+      "Fostered robust partnerships, amplifying Merit's technologies' market visibility and value.Proactively resolved customer concerns, ensuring unparalleled satisfaction and fostering a feedback-driven environment.Acted as the primary technical and project liaison, ensuring alignment between customer needs and global development strategies.",
+    technologiesUsed: ["Python", "React", "C++", "System Engineering"],
+  },
+  {
+    id: 3,
+    period: "2016-2020",
+    employer: "BCS Automotive Interface Solutions",
+    employerLocation: "Radolfzell, Germany",
+    position: "Technical Project Leader",
+    jobDescription:
+      " Orchestrated and directed the successful development of two pivotal projects for a prestigious German OEM. Strategically planned and executed project deliverables, proficiently delegating responsibilities and fostering seamless team collaboration. Ensured the successful integration of Mechanical and Hardware concepts with Software solutions for project success.",
+    technologiesUsed: ["Python", "React", "C++", "System Engineering"],
+  },
+  {
+    id: 4,
+    period: "2013-2016",
+    employer: "ZF Group",
+    employerLocation: "Czestochowa, Poland",
+    position: "Mechatronics Design Engineer",
+    jobDescription:
+      "Responsible for Mechanical design of entire Mechatronics switch projects for German OEMs.Coordinated all aspects of production, fabrication and operation of product designs. Ensured that the designs meet all the customer requirements. Organized and analyzed test reports.",
+    technologiesUsed: ["Python", "React", "C++", "System Engineering"],
+  },
+  {
+    id: 5,
+    period: "2010-2013",
+    employer: "Brose Group",
+    employerLocation: "Pune, India",
+    position: "Design Engineer",
+    jobDescription:
+      "Led mechanical design activities of Window Lifter projects for Indian OEMs. Developed complex cable-guided window lifters according to Customer requirements. Made sure that the product design meets the cost targets of the project. Provided end to end design engineering support to engineering, production and application departments of the Business unit.",
+    technologiesUsed: ["Python", "React", "C++", "System Engineering"],
+  },
+  {
+    id: 6,
+    period: "2008-2010",
+    employer: "Faurecia",
+    employerLocation: "Pune, India",
+    position: "Project Engineer",
+    jobDescription:
+      " Created designs for vehicle interior components such as instrument panel, door panels and center consoles. Made design changes according to the instructions from Faurecia France. Create detailed technical drawings for the components to be produced.",
+    technologiesUsed: ["Python", "React", "C++", "System Engineering"],
   },
 ];
